fb5f37aa8ac0656015104d531198762713f4332d
Erik Konner <erikkonner@gmail.com>
2019-03-28 00:11:31 -0600

    added properties to daily double class

--- src/DailyDouble.js
+++ src/DailyDouble.js
@@ -1,11 +1,13 @@
-import Rounds from './Rounds.js'
 import Clue from './Clue.js'
 
 class DailyD extends Clue {
   constructor(clue) {
     super(clue);
-    
-
+    this.questoin = clue.question;
+    this.pointValue = clue.pointValue;
+    this.answer = clue.answer;
+    this.categoryId = clue.categoryId;
+    this.dailyDouble = true;
   }
 }
 
