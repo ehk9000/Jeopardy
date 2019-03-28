9480e7ac12f7eef478bdbcf1db3f0675a03831d9
Erik Konner <erikkonner@gmail.com>
2019-03-28 12:07:04 -0600

    Added a hook to the game tests

--- test/sample-test.js
+++ test/sample-test.js
@@ -5,23 +5,27 @@ import Game from '../src/Game.js';
 import Rounds from '../src/Rounds.js'
 
 describe('Game', function() {
+  var game;
+  beforeEach(function() {
+     game = new Game('Erik', 'Aidan', 'Sean');
+    });
   it('should take player names ', function() {
-    let game = new Game('Erik', 'Aidan', 'Sean');
     expect(game.p1.playerName).to.equal('Erik');
     expect(game.p2.playerName).to.equal('Aidan');
     expect(game.p3.playerName).to.equal('Sean');
   });
+
   it('player dollar amount should be 0 by default', function() {
-    let game = new Game('Erik', 'Aidan', 'Sean');
     expect(game.p1.playerDollarAmount).to.equal(0);
     expect(game.p2.playerDollarAmount).to.equal(0);
     expect(game.p3.playerDollarAmount).to.equal(0);
   });
   it('player List should have a length of 3', function() {
-    let game = new Game('Erik', 'Aidan', 'Sean');
     expect(game.playerList).to.have.lengthOf(3);
   });
-  it('should find right index of topic array', function() {
-    let game
+
+  it('should start with 16 clues remaining by default, then decrement on clue click', function() {
+    expect(game.cluesRemaining).to.equal(16);
   });
+
 });
